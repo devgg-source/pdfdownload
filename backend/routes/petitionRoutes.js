ea09d6eb655362c5c02029e58e39d87c586a3928
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-15 15:16:54 +0530

    get all petitions route added

--- backend/routes/petitionRoutes.js
+++ backend/routes/petitionRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-import { addPetition } from "../controller/petitionController.js";
+import { addPetition, getPetitions } from "../controller/petitionController.js";
 
-router.route("/").post(addPetition);
+router.route("/").post(addPetition).get(getPetitions);
 
 export default router;
