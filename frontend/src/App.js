b69a3cce6a204a9518ad72f4d106cc6019118c68
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-16 17:05:20 +0530

    new form and table routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import PetitionList from "./components/PetitionList";
+import CreatePetition from "./components/CreatePetition";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>welcome to village App</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<PetitionList />} exact />
+        <Route path="/new" element={<CreatePetition />} />
+      </Routes>
+    </Router>
   );
 };
 
