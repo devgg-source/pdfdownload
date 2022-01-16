9ba122149d1711308dedfca8150a1eabae39513d
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-16 22:43:38 +0530

    Loader component created

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div class="text-center loader">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
