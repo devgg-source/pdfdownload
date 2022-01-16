f48a4c17fd681fc3243caf66fb1bdf4fc92e28bb
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-16 22:45:07 +0530

    download pdf functionality added

--- frontend/src/components/PetitionList.js
+++ frontend/src/components/PetitionList.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 import { listPetitions } from "../actions/petitionActions";
+import Loader from "./Loader";
 
 const PetitionList = () => {
   const dispatch = useDispatch();
@@ -13,42 +16,95 @@ const PetitionList = () => {
     dispatch(listPetitions());
   }, []);
 
+  const exportPDF = () => {
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "landscape"; // portrait or landscape
+
+    const marginLeft = 7;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(15);
+
+    const title = "Petition Report";
+    const headers = [
+      [
+        "SNO",
+        "NAME",
+
+        "COMMUNITY",
+        "AADHAR NUMBER",
+        "SMARTCARD NUMBER",
+        "FIRKKA",
+        "VILLAGE NAME",
+      ],
+    ];
+
+    const data = petitions.map((petition) => [
+      petition.sno,
+      petition.petitionerName,
+      petition.community,
+      petition.aadharNumber,
+      petition.smartCardNumber,
+      petition.firkka,
+      petition.villageName,
+    ]);
+
+    let content = {
+      startY: 50,
+      head: headers,
+      body: data,
+    };
+
+    doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    doc.save("report.pdf");
+  };
+
   return (
     <>
-      <table className="table table-striped table1">
-        <thead>
-          <tr>
-            <th>SNO</th>
-            <th>Petitioner Name</th>
-            <th>Community</th>
-            <th>Aadhar Number</th>
-            <th>Smart Card</th>
-            <th>Firkka</th>
-            <th>Village</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {petitions.map((petition) => (
-            <tr key={petition._id}>
-              <td>{petition.sno}</td>
-              <td>{petition.petitionerName}</td>
-              <td>{petition.community}</td>
-              <td>{petition.aadharNumber}</td>
-              <td>{petition.smartCardNumber}</td>
-              <td>{petition.firkka}</td>
-              <td>{petition.villageName}</td>
-              <td>
-                <i className="fas fa-edit"></i>
-              </td>
-              <td>
-                <i className="fa fa-trash"></i>
-              </td>
+      <button className="btn btn-primary pdf" onClick={exportPDF}>
+        {" "}
+        <i className="fa fa-file-pdf-o pd"></i> Download
+      </button>
+      {loading ? (
+        <Loader />
+      ) : (
+        <table className="table table-striped table1">
+          <thead>
+            <tr>
+              <th>SNO</th>
+              <th>Petitioner Name</th>
+              <th>Community</th>
+              <th>Aadhar Number</th>
+              <th>Smart Card</th>
+              <th>Firkka</th>
+              <th>Village</th>
+              <th></th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {petitions.map((petition) => (
+              <tr key={petition._id}>
+                <td>{petition.sno}</td>
+                <td>{petition.petitionerName}</td>
+                <td>{petition.community}</td>
+                <td>{petition.aadharNumber}</td>
+                <td>{petition.smartCardNumber}</td>
+                <td>{petition.firkka}</td>
+                <td>{petition.villageName}</td>
+                <td>
+                  <i className="fas fa-edit"></i>
+                </td>
+                <td>
+                  <i className="fa fa-trash"></i>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
