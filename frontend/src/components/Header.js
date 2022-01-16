f84ddd192e64d00ce35fcbd1b1a7a24e19b6c1a4
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-16 17:11:21 +0530

    Header component created

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light topnav">
+      <div className="container-fluid">
+        <h2 className="navbar-brand">Munkalam</h2>
+        <Link className="btn btn-create" to="/new">
+          <i className="fa fa-plus"></i> New Record
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
