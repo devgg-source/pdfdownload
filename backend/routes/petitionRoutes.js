5aee1f9bc69b9aabc1854f54029e8ddf60c1310d
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-18 13:51:38 +0530

    update and deleteAll routes added

--- backend/routes/petitionRoutes.js
+++ backend/routes/petitionRoutes.js
@@ -1,8 +1,22 @@
 import express from "express";
 const router = express.Router();
 
-import { addPetition, getPetitions } from "../controller/petitionController.js";
+import {
+  addPetition,
+  deletePetitionById,
+  getPetitions,
+  updatePetitionById,
+  getPetitionById,
+  deleteAll,
+} from "../controller/petitionController.js";
 
 router.route("/").post(addPetition).get(getPetitions);
+router
+  .route("/:id")
+  .delete(deletePetitionById)
+  .put(updatePetitionById)
+  .get(getPetitionById);
+
+router.route("/deleteAll").post(deleteAll);
 
 export default router;
