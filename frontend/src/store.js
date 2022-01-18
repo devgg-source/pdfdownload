6c23040cb5ab0b7dca18285115745eb2ebc2d731
devgg-source <ggkarthikmessi007@gmail.com>
2022-01-18 14:03:05 +0530

    update and deleteAll reducer connected

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,11 +4,19 @@ import thunk from "redux-thunk";
 import {
   petitionListReducer,
   petitionCreateReducer,
+  petitionDeleteReducer,
+  petitionDetailsReducer,
+  petitionUpdateReducer,
+  deleteAllPetitionsReducer,
 } from "./reducers/petitionReducers";
 
 const reducer = combineReducers({
   petitionList: petitionListReducer,
   createPetition: petitionCreateReducer,
+  petitionDelete: petitionDeleteReducer,
+  petitionDetails: petitionDetailsReducer,
+  petitionUpdate: petitionUpdateReducer,
+  petitionDeleteAll: deleteAllPetitionsReducer,
 });
 
 const middleware = [thunk];
